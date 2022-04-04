4245baa36a5b0a521846ebbf8b50aee8d0383272
therauch1 <kaj@r4uch.com>
2022-04-04 20:31:41 +0200

    initial commit of project and examples

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import FirstChart from "./lib/FirstChart.svelte";
+  import SecondChart from "./lib/SecondChart.svelte"
+  import "bootstrap/dist/css/bootstrap.min.css";
+</script>
+
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col" />
+      <div class="col-6">
+        <h1>First chart</h1>
+        <FirstChart />
+      </div>
+      <div class="col" />
+    </div>
+
+    <div class="row">
+      <div class="col" />
+      <div class="col-6">
+        <h1>Second chart</h1>
+        <SecondChart />
+      </div>
+      <div class="col" />
+    </div>
+  </div>
+</main>
+
+<style>
+</style>
---
+++ src/lib/FirstChart.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Plotly from "plotly.js-dist-min";
+  import { onMount } from "svelte";
+
+    onMount(() => {
+        Plotly.newPlot(
+            "firstChart",
+            /* JSON object */ {
+                data: [{ y: [1, 2, 3] }],
+                layout: { width: 600, height: 400 },
+            }
+        );
+    });
+</script>
+
+<div id="firstChart" />
+
+<style>
+</style>
