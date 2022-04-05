f714d32baf1a4e33bbfa071994ef83a74429027c
therauch1 <kaj@r4uch.com>
2022-04-05 23:15:13 +0200

    added functionality to switch chart with dropdown

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,31 @@
 <script>
   import FirstChart from "./lib/FirstChart.svelte";
-  import SecondChart from "./lib/SecondChart.svelte"
+  import SecondChart from "./lib/SecondChart.svelte";
   import "bootstrap/dist/css/bootstrap.min.css";
+
+  let charts = [
+    { id: 1, name: "First chart", component: FirstChart },
+    { id: 2, name: "Second chart", component: SecondChart },
+  ];
+  let showingChart = charts[0];
 </script>
 
 <main>
   <div class="container">
     <div class="row">
       <div class="col" />
-      <div class="col-6">
-        <h1>First chart</h1>
-        <FirstChart />
-      </div>
-      <div class="col" />
-    </div>
-
-    <div class="row">
-      <div class="col" />
-      <div class="col-6">
-        <h1>Second chart</h1>
-        <SecondChart />
+      <div class="col-8">
+        <select
+          bind:value={showingChart}
+          class="form-select"
+          aria-label="Default select example"
+        >
+          {#each charts as chart}
+            <option value={chart}>{chart.name}</option>
+          {/each}
+        </select>
+        <h1>{showingChart.name}</h1>
+        <svelte:component this={showingChart.component} />
       </div>
       <div class="col" />
     </div>
--- src/lib/FirstChart.svelte
+++ src/lib/FirstChart.svelte
@@ -7,7 +7,6 @@
             "firstChart",
             /* JSON object */ {
                 data: [{ y: [1, 2, 3] }],
-                layout: { width: 600, height: 400 },
             }
         );
     });
