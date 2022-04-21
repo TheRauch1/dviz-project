20fe7b1de1497a1b013c7a22e02920ac418115eb
therauch1 <kaj@r4uch.com>
2022-04-21 23:48:21 +0200

    test with reddit api

--- src/lib/FirstChart.svelte
+++ src/lib/FirstChart.svelte
@@ -1,12 +1,43 @@
 <script>
-  import Plotly from "plotly.js-dist-min";
-  import { onMount } from "svelte";
+    import Plotly from "plotly.js-dist-min";
+    import { onMount } from "svelte";
 
-    onMount(() => {
+    const endpoint = "https://www.reddit.com/r/sysadmin/rising.json"
+    let posts;
+
+    onMount(async function () {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        posts = data.data.children;
+        console.log(posts);
+
+        var subredditMap = {};
+        var x_data = [];
+        var y_data = [];
+
+        
+        posts.forEach(post => {
+            if (!(post.data.subreddit in subredditMap)) {
+                subredditMap[post.data.subreddit] = 1;
+                debugger;
+            } else {
+                subredditMap[post.data.subbreddit] += 1;
+            }
+        });
+
+        Object.entries(subredditMap).forEach(([key, value ]) => {
+            x_data.push(key);
+            y_data.push(value);
+        });
+
         Plotly.newPlot(
             "firstChart",
             /* JSON object */ {
-                data: [{ y: [1, 2, 3] }],
+                data: [{
+                    x: x_data,
+                    y: y_data,
+                    type: 'bar'
+                }],
             }
         );
     });
